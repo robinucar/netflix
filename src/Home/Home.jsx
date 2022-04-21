f150a1cf16a8cdc202e400c77dc134df78aae4e8
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-21 17:24:47 +0100

    List, ListItem component

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,3 +1,4 @@
+import List from '../components/List/List'
 import Featured from "../components/Featured/Featured";
 import Navbar from "../components/Navbar/Navbar";
 import "./home.scss";
@@ -6,7 +7,11 @@ const Home = () => {
   return (
     <div className="home">
       <Navbar />
-      <Featured type="" />
+      <Featured type="movie" />
+      <List />
+      <List />
+      <List />
+      <List />
     </div>
   );
 };
